1d03f9a277eac7ac7ac678bb2888b1c095456de0
ciaarraa <ciara.tully@immersivelabs.com>
2022-07-29 22:02:30 +0000

    Hook up dynamo db
    Create an aws config file and specify the aws config. This still
    needs to be connected to aws using credentials.
    
    Add a listing model and create a table in dynamo db.

---
+++ app/models/listing.rb
@@ -0,0 +1,11 @@
+class Listing < ApplicationRecord
+    include Dynamoid::Document
+
+    table name: listing, key: id, capacity_mode: on_demand
+    
+    field :author_id :number
+    field :title :string
+    field :description :string
+    field :price :number
+    feild :category :string
+end
---
+++ db/migrate/20220729213424_create_listing.rb
@@ -0,0 +1,8 @@
+class CreateListing < ActiveRecord::Migration[7.0]
+  def change
+    create_table :listings do |t|
+
+      t.timestamps
+    end
+  end
+end
