9d06feaec6475a300cb3eb4812361fea772a70ac
ciaarraa <ciara.tully@immersivelabs.com>
2022-10-11 14:32:53 +0000

    If the user is logged in, show a top navigation bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  layout :layout_by_resource
+
+  def layout_by_resource
+    user_signed_in? ? "application" : "devise"
+  end
 end
